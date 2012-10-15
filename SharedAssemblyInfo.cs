f0e4c6478712e9c54a08a3daa3f74a55d37d9cb2
Robin van der Knaap <robin@webpirates.nl>
2012-10-15 16:10:25 +0200

    Refactored build en nuget scripts to fit teamcity

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,93 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#region Smurf
+//                                        _,,aaaaa,,_
+//                                    _,dP"''    `""""Ya,_
+//                                ,aP"'                `"Yb,_
+//                                ,8"'                       `"8a,
+//                            ,8"                             `"8,_
+//                            ,8"                                  "Yb,
+//                        ,8"                                      `8,
+//                        dP'                                        8I
+//                    ,8"                           bg,_          ,P'
+//                    ,8'                              "Y8"Ya,,,,ad"
+//                    ,d"                            a,_ I8   `"""'
+//                    ,8'                              ""888
+//                    dP     __                           `Yb,
+//                dP'  _,d8P::::Y8b,                     `Ya
+//            ,adba8',d88P::;;::;;;:"b:::Ya,_               Ya
+//            dP":::"Y88P:;P"""YP"""Yb;::::::"Ya,             "Y,
+//            8:::::::Yb;d" _  "_    dI:::::::::"Yb,__,,gd88ba,db
+//            Yb:::::::"8(,8P _d8   d8:::::::::::::Y88P"::::::Y8I
+//            `Yb;:::::::""::"":b,,dP::::::::::::::::::;aaa;:::8(
+//            `Y8a;:::::::::::::::::::::;;::::::::::8P""Y8)::8I
+//                8b"ba::::::::::::::::;adP:::::::::::":::dP::;8'
+//                `8b;::::::::::::;aad888P::::::::::::::;dP::;8'
+//                `8b;::::::::""""88"  d::::::::::b;:::::;;dP'
+//                    "Yb;::::::::::Y8bad::::::::::;"8Paaa""'
+//                    `"Y8a;;;:::::::::::::;;aadP""
+//                        ``""Y88bbbdddd88P""8b,
+//                                _,d8"::::::"8b,
+//                                ,dP8"::::::;;:::"b,
+//                            ,dP"8:::::::Yb;::::"b,
+//                            ,8P:dP:::::::::Yb;::::"b,
+//                        _,dP:;8":::::::::::Yb;::::"b
+//                ,aaaaaa,,d8P:::8":::::::::::;dP:::::;8
+//            ,ad":;;:::::"::::8"::::::::::;dP::::::;dI
+//            dP";adP":::::;:;dP;::::aaaad88"::::::adP:8b,___
+//        d8:::8;;;aadP"::8'Y8:d8P"::::::::::;dP";d"'`Yb:"b
+//        8I:::;""":::::;dP I8P"::::::::::;a8"a8P"     "b:P
+//        Yb::::"8baa8"""'  8;:;d"::::::::d8P"'         8"
+//            "YbaaP::8;P      `8;d::;a::;;;;dP           ,8
+//            `"Y8P"'         Yb;;d::;aadP"           ,d'
+//                                "YP:::"P'             ,d'
+//                                "8bdP'    _        ,8'
+//                Normand         ,8"`""Yba,d"      ,d"
+//                Veilleux       ,P'     d"8'     ,d"
+//                            ,8'     d'8'     ,P'
+//                            (b      8 I      8,
+//                                Y,     Y,Y,     `b,
+//                        ____   "8,__ `Y,Y,     `Y""b,
+//                    ,adP""""b8P""""""""Ybdb,        Y,
+//                    ,dP"    ,dP'            `""       `8
+//                ,8"     ,P'                        ,P
+//                8'      8)                        ,8'
+//                8,      Yb                      ,aP'
+//                `Ya      Yb                  ,ad"'
+//                    "Ya,___ "Ya             ,ad"'
+//                    ``""""""`Yba,,,,,,,adP"'
+//                                `"""""""'
+#endregion
+
+
+// This shared assembly info should be used by all projects within the solution. 
+// Make sure you add (as link!!) this file to all projects in the solution.
+// This makes sure some properties which should be equal for all projects are managed in one place
+// http://blogs.msdn.com/b/jjameson/archive/2009/04/03/shared-assembly-info-in-visual-studio-projects.aspx
+
+[assembly: AssemblyProduct("AutoPoco.DataSources")]
+[assembly: AssemblyDescription("Additional datasources for AutoPoco, including enum, city, street, postal, company, datetime, timespan")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Revision
+//      Build
+//
+// Version should always be 0.0.0.0, version number is determined by build server
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
+ 
